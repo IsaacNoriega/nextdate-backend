44fe0098e99784b153b6f152d2224eaeef044799
unknown <isaac.noriega@iteso.mx>
2026-05-24 16:54:10 -0600

    feat(auth): configure bcrypt password encoder and public endpoints

---
+++ src/main/java/com/nextdate/backend/auth/infrastructure/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.nextdate.backend.auth.infrastructure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  // Definimos el bean de encoding de contraseñas con BCrypt
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  // Definimos las reglas de acceso a las rutas
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    http
+        // Deshabilitamos CSRF porque no usaremos sesiones
+        .csrf(AbstractHttpConfigurer::disable)
+        // Autenticación HTTP
+        .authorizeHttpRequests(
+            auth ->
+                auth.requestMatchers(
+                        "/graphql", "/graphiql") // Rutas que no requieren autenticación
+                    .permitAll() // Permite el acceso a estas rutas
+                    .anyRequest()
+                    .authenticated() // Todas las demás rutas requieren autenticación
+            );
+
+    return http.build();
+  }
+}
