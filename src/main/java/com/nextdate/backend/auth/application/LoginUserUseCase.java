6a899ac66838c9c64920d4bba3a3e8f8576b25ef
unknown <isaac.noriega@iteso.mx>
2026-05-24 16:54:17 -0600

    feat(auth): define login usecase and token service ports

---
+++ src/main/java/com/nextdate/backend/auth/application/LoginUserUseCase.java
@@ -0,0 +1,12 @@
+package com.nextdate.backend.auth.application;
+
+import com.nextdate.backend.auth.domain.User;
+
+public interface LoginUserUseCase {
+
+  LoginResult login(LoginCommand command); // Command con los datos del usuario
+
+  record LoginCommand(String email, String password) {} // Command con los datos del usuario
+
+  record LoginResult(User user, String token) {} // Result con los datos del usuario y el token
+}
