3aa78ca27139f873f71873a580f19727052f9c56
unknown <isaac.noriega@iteso.mx>
2026-09-01 14:44:34 -0600

    test(backend): add JWT tests, in-memory H2 configuration and enable full context test

--- src/test/java/com/nextdate/backend/BackendApplicationTests.java
+++ src/test/java/com/nextdate/backend/BackendApplicationTests.java
@@ -1,13 +1,13 @@
 package com.nextdate.backend;
 
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class BackendApplicationTests {
 
   @Test
-  @Disabled("Requiere una instancia activa de PostgreSQL/PostGIS corriendo en localhost")
+  @DisplayName("Debería cargar el contexto completo de Spring Boot correctamente")
   void contextLoads() {}
 }
